0ecf82596612edbbb57c1fae2e89a090b56513c4
Veer Sandhu <sandhu.s.veer@gmail.com>
2022-03-01 23:41:40 -0500

    action piece links

--- apps/home.py
+++ apps/home.py
@@ -25,12 +25,13 @@ def app():
 
     st.markdown('*Data Vision* is a Non-Governmental Organization fighting against data driven surveillance for civil liberties and digital rights. \
         This app hopes to bring awarness to the technologies behind internet surveillance so the general public becomes involved with the issue. \
-        This app runs **simulations** of data tracking software but does not affect the users privacy as the data not been gathered from a real source (it\'s simply synthetic). \
+        This app runs **simulations** of data tracking software but does not affect the users\' privacy as the data is synthetic. \
         Visit the links below to view the organization\'s social media and other action pieces.')
 
     st.markdown('***Organization Links***')
     st.markdown("""
     - [Instagram](https://www.instagram.com/true.datavision/) 
-    - [Petition w/ OpenMedia](https://action.openmedia.org/page/95077/petition/1)
+    - [#DemandPrivacy Petition w/ OpenMedia](https://action.openmedia.org/page/95077/petition/1)
+    - [#NoMobileTracking Petition w/ OpenMedia](https://action.openmedia.org/page/97956/action/1?ea.tracking.id=om)
     - [Github](https://github.com/True-Data-Vision)
     """)
